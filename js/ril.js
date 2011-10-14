c1860b66e4567ec17337aa508d51d5e4d2a9322c
Miu Kobayashi <miukoba@gmail.com>
2011-10-15 04:45:15 +0900

    add all

---
+++ js/ril.js
@@ -0,0 +1,18 @@
+function onComplete(feed)
+{
+    var temp = '<ul>';
+    for(var i = 0; i < feed.items.length; i++) {
+        var item = feed.items[i];
+        var url = item.link;
+        var title = item.title;
+        temp +='<li>';
+        temp +='<a class="ril" href="http://readitlaterlist.com/edit_process.php?url=' + url + '&title=' + title + '" target="readitlateriframe">RIL!</a>' + '<span class="title">' + title + '</span>';
+        temp +='<div class="description">' + item.description + '</div>';
+        temp +='</li>';
+    }
+    temp += '</ul>';
+    jQuery('#news').append(temp);
+
+    jQuery('#news').append('<iframe style="display:none;" src="" width="0" height="0" name="readitlateriframe"></iframe>');
+} 
+
---
+++ proxy.php
@@ -0,0 +1,13 @@
+<?php
+header('Content-type: application/xml');
+$handle = fopen($_REQUEST['url'], "r");
+
+if ($handle) {
+        while (!feof($handle)) {
+                    $buffer = fgets($handle, 4096);
+                            echo $buffer;
+                        }
+            fclose($handle);
+}
+?>
+
